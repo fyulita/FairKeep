f0e40939d712e0351ad332d4bbb1b40d0ef38d0e
Federico Yulita <federico_yulita@hotmail.com>
2024-12-15 20:47:52 -0300

    Created a superuser and added 2 test expenses

--- expense_tracker/expenses/models.py
+++ expense_tracker/expenses/models.py
@@ -16,7 +16,7 @@ class Expense(models.Model):
     name = models.CharField(max_length=50)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    date = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} - {self.amount}"
--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Expense
+from .serializers import ExpenseSerializer
 
 # Create your views here.
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()  # Define what data to query
+    serializer_class = ExpenseSerializer  # Link the serializer
