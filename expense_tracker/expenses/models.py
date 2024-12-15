e97b55b4ea175d870ec241f6c00c79438e42a932
Federico Yulita <federico_yulita@hotmail.com>
2024-12-15 20:29:16 -0300

    Initial commit: Set up Django project with expenses app

---
+++ expense_tracker/expenses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('Home Supplies', 'Home Supplies'),
+        ('Food', 'Food'),
+        ('Transport', 'Transport'),
+        ('Entertainment', 'Entertainment'),
+        ('Periodic Expenses', 'Periodic Expenses'),
+        ('Other', 'Other'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} - {self.amount}"
